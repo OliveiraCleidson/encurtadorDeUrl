a199fb7d935c73bc174c9310e5eb878bf8896db8
Cleidson Oliveira <gk.cleidson@yahoo.com.br>
2021-02-23 16:49:48 -0300

    refactor: shortcutGenerator working

--- src/modules/shortcut/services/shortcutGenerator.service.ts
+++ src/modules/shortcut/services/shortcutGenerator.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { v4 } from 'uuid';
 
 @Injectable()
 export class ShortcutGeneratorService {
   execute(): string {
-    return 'a';
+    const code = this.createCode();
+
+    return code;
+  }
+
+  private parseInputToAlphaNumeric(data: string): string {
+    return data.replace(/\W/g, '');
+  }
+
+  private createCode(): string {
+    const randomString = this.parseInputToAlphaNumeric(v4());
+    const randomString2 = this.parseInputToAlphaNumeric(v4());
+
+    const part1 = this.getFirstTeenCharacterOfAString(randomString);
+
+    const part2 = this.getFirstTeenCharacterOfAString(randomString2);
+
+    return `${part1}${part2}`;
+  }
+
+  private getFirstTeenCharacterOfAString(data: string) {
+    return data.substring(0, data.length >= 5 ? 5 : data.length - 1);
   }
 }

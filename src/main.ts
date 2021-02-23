6e9c6783facd86164be5cf502fcf4e22f8c7297e
Cleidson Oliveira <gk.cleidson@yahoo.com.br>
2021-02-23 16:00:25 -0300

    feat: add dotenv and reflect metadata

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import 'dotenv/config';
+import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();

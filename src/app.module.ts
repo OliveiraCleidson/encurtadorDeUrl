c26f2c0402b06246b665216c4f0b3c4e620990ae
Cleidson Oliveira <gk.cleidson@yahoo.com.br>
2021-02-23 15:59:36 -0300

    refactor: remove app service,controller, controller spc

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ShortcutModule } from './modules/shortcut/shortcut.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ShortcutModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
